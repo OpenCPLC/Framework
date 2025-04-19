0f2b03ce6cdacbc7b77b0bb8dc5ad2e8de0def8d
Xaeian <git@sqrt.pl>
2025-04-19 23:36:35 +0200

    More samples...

--- res/samples/twi/shtc3/main.c
+++ res/samples/twi/shtc3/main.c
@@ -1,7 +1,11 @@
 #include "opencplc.h" // Import funkcji sterownika
 #include "shtc3.h" // Interfejs do obsługi czujnika SHTC3
 
-// Wątek aplikacji
+/**
+ * @name  Projekt: twi/shtc3
+ * @brief Odczytywane są wartości potencjometru oraz napięcie zasilania
+ */
+
 void loop(void)
 {
   while(1) {
@@ -21,15 +25,15 @@ thread_fnc(SHTC3_Thread, __NOP, SHTC3_Loop)
 
 stack(stack_plc, 256); // Stos pamięci dla wątku PLC
 stack(stack_dbg, 256); // Stos pamięci dla wątku debug'era (logs + bash)
-stack(stack_loop, 1024); // Stos pamięci dla funkcji loop
 stack(stack_shtc, 128); // Stos pamięci dla czujnika SHTC3
+stack(stack_loop, 1024); // Stos pamięci dla funkcji loop
 
 int main(void)
 {
   thread(PLC_Thread, stack_plc); // Dodanie wątku sterownika
   thread(DBG_Loop, stack_dbg); // Dodanie wątku debug'era (logs + bash)
+  thread(SHTC3_Thread, stack_shtc); // Dodanie funkcji obsługującej czujnik SHTC3
   thread(loop, stack_loop); // Dodanie funkcji loop jako wątek
-  thread(SHTC3_Thread, stack_shtc); // Dodanie funkcji loop jako wątek
   vrts_init(); // Włączenie systemy przełączania wątków VRTS
   while(1); // W to miejsce program nigdy nie powinien dojść
 }
